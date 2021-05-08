94501ba45f7c9fc272b09c14e3f3b9f4ccb2ec7f
Youssef samih <youssef.samih@4d.com>
2021-05-08 18:51:06 +0000

    feat: finish error management

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,21 +1,20 @@
 import express from "express";
-import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { CustomError } from "../errors/custom-error";
 
-export const errorHandler = (err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
-  if (err instanceof RequestValidationError) {
-    const formattedErrors = err.errors.map(error => {
-      return {
-        message: error.msg,
-        field: error.param
-      };
-    });
-    return res.status(400).send({ errors: formattedErrors });
-  }
-  if (err instanceof DatabaseConnectionError) {
-    console.log('DatabaseConnectionError')
+export const errorHandler = (
+  err: Error,
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
   res.status(400).send({
-    message: err.message
-  })
-}
+    errors: [
+      {
+        message: "Something went wrong",
+      },
+    ],
+  });
+};
