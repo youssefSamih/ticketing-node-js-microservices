fafda19860af37cd1477bddb7d22f6971b476fde
Youssef samih <youssef.samih@4d.com>
2021-08-15 01:27:30 +0100

    feat: begin testing 10

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 
 import { errorHandler } from "./middlewares/error-handler";
 import { currentUserRouter } from "./routes/current-user";
@@ -11,7 +12,12 @@ import { signupRouter } from "./routes/signup";
 import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
+app.set('trust proxy', true)
 app.use(json());
+app.use(cookieSession({
+  signed: false,
+  secure: true
+}));
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -25,6 +31,9 @@ app.all('*', async () => {
 app.use(errorHandler);
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
   try {
     await mongoose.connect('mongodb://auth-mongo-srv/auth', {
       useNewUrlParser: true,
--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { Password } from '../services/password';
 
 // interface describe props required to create new user
 interface UserAttrs {
@@ -26,7 +27,25 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true
   }
+},
+{
+  toJSON: {
+    transform(doc, ret){
+      ret.id = ret._id;
+      delete ret._id;
+      delete ret.password;
+      delete ret.__v
+    }
+  }
 });
+
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
+})
 
 userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs)
